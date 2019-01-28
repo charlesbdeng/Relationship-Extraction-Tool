92c61ae6c5c79daab82c667dfbf74719c08fd7a6
Charles Deng <Charlesdeng98@gmail.com>
2019-01-28 13:56:23 -0500

    add wrap

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
 # from flask_httpauth import HTTPBasicAuth
 #
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route("/")
+@cross_origin()
 def home():
     return "Hello, World!"
 if __name__ == "__main__":
