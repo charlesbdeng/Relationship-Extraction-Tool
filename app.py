ca61e1eef453b648bae74acb9a23357262d803c4
Charles Deng <Charlesdeng98@gmail.com>
2019-01-28 13:49:48 -0500

    broken

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
 from flask_httpauth import HTTPBasicAuth
 
-from relation_extraction import relex
 from flask_cors import CORS
 app = Flask(__name__)
 @app.route("/")
