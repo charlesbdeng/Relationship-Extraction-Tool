5873d6ead38acb2f46d936a4a102c898f1cfcbca
Charles Deng <Charlesdeng98@gmail.com>
2019-01-28 13:58:55 -0500

    example

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify, abort, make_response, request, url_for
-# from flask_httpauth import HTTPBasicAuth
-#
-from flask_cors import CORS,cross_origin
+from flask import Flask
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 @cross_origin()
-def home():
-    return "Hello, World!"
-if __name__ == "__main__":
-    app.run(debug=True)
+def helloWorld():
+  return "Hello, cross-origin-world!"
