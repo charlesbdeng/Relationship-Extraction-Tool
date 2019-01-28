b2294edec0e237551e864a06715c5e9c169387ea
Charles Deng <Charlesdeng98@gmail.com>
2019-01-28 13:47:51 -0500

    check

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
+
+from relation_extraction import relex
+from flask_cors import CORSapp = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World!"
