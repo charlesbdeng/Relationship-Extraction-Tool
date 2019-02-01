9e3b68783675dd81000171213163733b43eef9ee
Charles Deng <Charlesdeng98@gmail.com>
2019-02-01 16:21:38 -0500

    cleaned up comments

--- app.py
+++ app.py
@@ -18,30 +18,6 @@ app = Flask(__name__)
 CORS(app)
 
 
-
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-# @auth.login_required
-def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
-
-
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
@@ -52,21 +28,6 @@ def get_task(task_id):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-# @app.route('/todo/api/v1.0/tasks', methods=['POST'])
-# def create_task():
-#     if not request.json or not 'title' in request.json:
-#         abort(400)
-#     task = {
-#         'id': tasks[-1]['id'] + 1,
-#         'title': request.json['title'],
-#         'description': request.json.get('description', ""),
-#         'done': False
-#     }
-#     tasks.append(task)
-#     return jsonify({'task': task}), 201
-
 
 @app.route('/api/entity_entity', methods = ["POST"])
 def ent_ent():
@@ -85,8 +46,6 @@ def ent_ent():
 
 @app.route('/api/relation_entity', methods = ["POST"])
 def rel_ent():
-    # if not request.json or len(request.json["text"]) == 0:
-    #     abort(400)
     object = request.json
     text = object["text"]
     ent_values = object['entity_values']
@@ -95,20 +54,9 @@ def rel_ent():
     rel_name = object['relation_name']
     scope = object['scope']
     entities = relation_entity(ent_name,rel_name, ent_values, rel_values,text,scope)
-    # print(type(ent2_vals))
-    # print(type(ent1_name))
     return jsonify({"data":entities})
-
-    # return jsonify({"data":object})
-
-
-    # return jsonify({"data": object})
-
-
 @app.route('/api/all_entities_relations', methods = ["POST"])
 def all():
-    # if not request.json or len(request.json["text"]) == 0:
-    #     abort(400)
     object = request.json
     text = object["text"]
     ent1_vals = object['entity_1_values']
@@ -119,11 +67,8 @@ def all():
     rel_name = object["relation_name"]
     scope = object['scope']
     entities = all_ent_rel(ent1_name, ent2_name, rel_name, ent1_vals,ent2_vals, relation_vals, text, scope)
-    # print(type(ent2_vals))
-    # print(type(ent1_name))
     return jsonify({"data":entities})
 
-    # return jsonify({"text":text}),200
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
@@ -141,7 +86,6 @@ def update_task(task_id):
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify({'task': task[0]})
-
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
